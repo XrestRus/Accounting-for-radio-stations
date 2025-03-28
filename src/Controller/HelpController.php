86b3501afed4c59460c263601f78d09d4ffed469
Nikita <xrestrus@gmail.com>
2025-03-28 17:11:40 +0700

    10. Экран инструкции и справки

--- src/Controller/HelpController.php
+++ src/Controller/HelpController.php
@@ -6,12 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Контроллер для работы со страницей справки и инструкциями
+ */
 class HelpController extends AbstractController
 {
+    /**
+     * Отображает основную страницу справки
+     */
     #[Route('/help', name: 'app_help')]
     public function index(): Response
     {
         return $this->render('help/index.html.twig', [
+            'controller_name' => 'HelpController',
+        ]);
+    }
+    
+    /**
+     * Отображает печатную версию справки
+     */
+    #[Route('/help/print', name: 'app_help_print')]
+    public function printHelp(): Response
+    {
+        return $this->render('help/print.html.twig', [
             'controller_name' => 'HelpController',
         ]);
     }
