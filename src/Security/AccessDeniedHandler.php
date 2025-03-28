f41cf4c5c010fee5fd02127fede83c53a31cf1fb
Nikita <xrestrus@gmail.com>
2025-03-28 18:25:22 +0700

    Доработки

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+/**
+ * Обработчик запретов доступа
+ * 
+ * Перенаправляет на специальную страницу с информацией о запрете доступа
+ */
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * Обрабатывает запрет доступа
+     * 
+     * @param Request $request
+     * @param AccessDeniedException $accessDeniedException
+     * @return Response|null
+     */
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        return new RedirectResponse($this->urlGenerator->generate('app_access_denied'));
+    }
+} 
---
+++ src/Service/LogService.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Device;
+use App\Entity\Log;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * Сервис для логирования операций
+ */
+class LogService
+{
+    private EntityManagerInterface $entityManager;
+    private Security $security;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        Security $security
+    ) {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
+    /**
+     * Создает запись лога для операции
+     * 
+     * @param string $action Название действия
+     * @param Device|null $device Устройство (опционально)
+     * @param string|null $details Детали операции (опционально)
+     * @param array|null $detailsMeta Мета-данные операции (опционально)
+     * @return Log Созданный лог
+     */
+    public function log(string $action, ?Device $device = null, ?string $details = null, ?array $detailsMeta = null): Log
+    {
+        /** @var User $user */
+        $user = $this->security->getUser();
+        
+        $log = new Log();
+        $log->setUser($user);
+        $log->setAction($action);
+        
+        if ($device) {
+            $log->setDevice($device);
+        }
+        
+        if ($details) {
+            $log->setDetails($details);
+        }
+        
+        if ($detailsMeta) {
+            $log->setDetailsMeta($detailsMeta);
+        }
+        
+        $this->entityManager->persist($log);
+        $this->entityManager->flush();
+        
+        return $log;
+    }
+    
+    /**
+     * Логирует операцию добавления устройства
+     * 
+     * @param Device $device Добавленное устройство
+     * @return Log Созданный лог
+     */
+    public function logDeviceCreate(Device $device): Log
+    {
+        return $this->log(
+            'Добавление устройства',
+            $device,
+            sprintf('Добавлено устройство "%s" (S/N: %s)', $device->getName(), $device->getSerialNumber())
+        );
+    }
+    
+    /**
+     * Логирует операцию редактирования устройства
+     * 
+     * @param Device $device Отредактированное устройство
+     * @return Log Созданный лог
+     */
+    public function logDeviceEdit(Device $device): Log
+    {
+        return $this->log(
+            'Редактирование устройства',
+            $device,
+            sprintf('Отредактировано устройство "%s" (S/N: %s)', $device->getName(), $device->getSerialNumber())
+        );
+    }
+    
+    /**
+     * Логирует операцию изменения статуса устройства
+     * 
+     * @param Device $device Устройство
+     * @param string $oldStatus Предыдущий статус
+     * @param string $newStatus Новый статус
+     * @return Log Созданный лог
+     */
+    public function logDeviceStatusChange(Device $device, string $oldStatus, string $newStatus): Log
+    {
+        return $this->log(
+            'Изменение статуса устройства',
+            $device,
+            sprintf('Изменен статус устройства "%s" (S/N: %s) с "%s" на "%s"', 
+                $device->getName(), 
+                $device->getSerialNumber(),
+                $oldStatus,
+                $newStatus
+            )
+        );
+    }
+    
+    /**
+     * Логирует операцию выдачи устройства
+     * 
+     * @param Device $device Выданное устройство
+     * @param string $employeeName Имя сотрудника
+     * @param \DateTime $dueDate Дата возврата
+     * @return Log Созданный лог
+     */
+    public function logDeviceIssue(Device $device, string $employeeName, \DateTime $dueDate): Log
+    {
+        return $this->log(
+            'Выдача устройства',
+            $device,
+            sprintf('Устройство "%s" (S/N: %s) выдано сотруднику %s до %s', 
+                $device->getName(), 
+                $device->getSerialNumber(),
+                $employeeName,
+                $dueDate->format('d.m.Y')
+            )
+        );
+    }
+    
+    /**
+     * Логирует операцию возврата устройства
+     * 
+     * @param Device $device Возвращенное устройство
+     * @param string $employeeName Имя сотрудника
+     * @param string $returnStatus Статус возврата
+     * @return Log Созданный лог
+     */
+    public function logDeviceReturn(Device $device, string $employeeName, string $returnStatus): Log
+    {
+        return $this->log(
+            'Возврат устройства',
+            $device,
+            sprintf('Устройство "%s" (S/N: %s) возвращено сотрудником %s в состоянии "%s"', 
+                $device->getName(), 
+                $device->getSerialNumber(),
+                $employeeName,
+                $returnStatus
+            )
+        );
+    }
+    
+    /**
+     * Логирует операцию списания устройства
+     * 
+     * @param Device $device Списанное устройство
+     * @param string|null $comment Комментарий о списании
+     * @return Log Созданный лог
+     */
+    public function logDeviceWriteOff(Device $device, ?string $comment = null): Log
+    {
+        $details = sprintf('Устройство "%s" (S/N: %s) списано', 
+            $device->getName(), 
+            $device->getSerialNumber()
+        );
+        
+        if ($comment) {
+            $details .= sprintf('. Причина: %s', $comment);
+        }
+        
+        return $this->log('Списание устройства', $device, $details);
+    }
+} 
